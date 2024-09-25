bc66d0d61e0d361a0d675680d9fb0d97ad570862
Renan <renanmaziero07@gmail.com>
2024-09-25 16:42:08 -0300

    Cria pasta `importingMap` e cria o mapa a partir da constante `boardMap`

---
+++ lab/importingMap/script.js
@@ -0,0 +1,42 @@
+const boardMap = [
+    [ "#", "#", "#", "#", "#", "#", "#", "#" ],
+    [ "#", ".", ".", ".", ".", ".", ".", "#" ],
+    [ "#", ".", ".", ".", "#", ".", ".", "#" ],
+    [ "#", ".", "#", "G", ".", ".", ".", "#" ],
+    [ "#", ".", ".", "G", "B", "#", ".", "#" ],
+    [ "#", ".", ".", "#", ".", "B", ".", "#" ],
+    [ "#", ".", ".", "P", ".", ".", ".", "#" ],
+    [ "#", "#", "#", "#", "#", "#", "#", "#" ]
+]
+
+const DIST_SALTO = 66;
+const MARGIN_FIX = 4;
+const NUM_ROWS = boardMap[0].length;
+const NUM_COLS = boardMap.length;
+
+function rule0() {
+    return false;
+}
+
+buildBoard(NUM_ROWS, NUM_COLS);
+
+function buildBoard(qtd_row, qtd_cell) {
+    const game = document.getElementById('game');
+    const board = createGameElement('div', 'board', game);
+
+    for (let y = 0; y < qtd_row; y++) {
+        const linha = createGameElement('div', 'row', board);
+        
+        for (let x = 0; x < qtd_cell; x++) {
+            const celula = createGameElement('div', 'cell', linha);
+        }
+    }
+}
+
+function createGameElement(nomeElemento, nomeClasse, parentNode) {
+    const element = document.createElement(nomeElemento);
+    element.classList.add(nomeClasse);
+    parentNode.append(element);
+    
+    return element;
+}

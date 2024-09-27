07f24e41e24a53ffc46feb9c76c9f9ff30631a0d
Renan <renanmaziero07@gmail.com>
2024-09-27 17:06:40 -0300

    Move função `calculaPosicao` para classe `Piece`

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const NUM_ROWS = boardMap.length;
 const NUM_COLS = boardMap[0].length;
 
-const DIST_SALTO = 66;
-const MARGIN_FIX = 4;
-
 const pieces = buildGameBoard(NUM_ROWS, NUM_COLS);
 const board = document.querySelector('.board');
 
@@ -27,8 +24,4 @@ window.addEventListener("keydown", function (event) {
 function verifyPosition(position) {
     let { x, y } = position;
     return boardMap[x][y] !== "#";
-}
-
-function calculaPosicao(qtd) {
-    return qtd * DIST_SALTO + MARGIN_FIX + "px";
 }

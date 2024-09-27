bb47f933c9906a55c2654e991dbb3e0a39783696
Renan <renanmaziero07@gmail.com>
2024-09-27 14:48:19 -0300

    Atualização script

--- script.js
+++ script.js
@@ -11,16 +11,21 @@ const boardMap = [
 
 const NUM_ROWS = boardMap.length;
 const NUM_COLS = boardMap[0].length;
+
 const DIST_SALTO = 66;
 const MARGIN_FIX = 4;
 
 const pieces = buildGameBoard(NUM_ROWS, NUM_COLS);
 const player = new Player(pieces.player.x, pieces.player.y);
-const playerElement = document.querySelector('.player');
+const board = document.querySelector('.board');
+const playerElement = createGameElement('div', 'player', board);
+
 playerElement.style.top = calculaPosicao(player.x);
 playerElement.style.left = calculaPosicao(player.y);
+
 window.addEventListener("keydown", function (event) {
     const next = player.nextPosition(event.code);
+    
     if (verifyPosition(next)) {
         player.moveTo(next, playerElement);
     }
@@ -81,6 +86,5 @@ function buildGameBoard(linhas, celulas) {
         }
     }
     
-    createGameElement('div', 'player', board);
     return positionOfPieces;
 }

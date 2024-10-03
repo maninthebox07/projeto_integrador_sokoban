1a9da6b40bedda64fc180ca9a94567441ca56bb7
Renan <renanmaziero07@gmail.com>
2024-10-03 17:03:13 -0300

    Cria função `string2BoardMap`

--- src/board.js
+++ src/board.js
@@ -1,16 +1,35 @@
-export const boardMap = [
-    ["#", "#", "#", "#", "#", "#", "#", "#"],
-    ["#", ".", ".", ".", ".", ".", ".", "#"],
-    ["#", ".", ".", ".", "#", ".", ".", "#"],
-    ["#", ".", "#", ".", "G", ".", ".", "#"],
-    ["#", ".", ".", ".", "B", "#", ".", "#"],
-    ["#", ".", "G", "B", "P", "B", "G", "#"],
-    ["#", ".", ".", ".", ".", ".", ".", "#"],
-    ["#", "#", "#", "#", "#", "#", "#", "#"]
-]
+const lvl0 = `
+ __#####
+###P..#_
+#..B#.##
+#.#GB..#
+#..G.#.#
+#..#...#
+_#...###
+_#####
+`
+
+function string2BoardMap(level) {
+    const lines = level.trim().split('\n');
+    console.log(lines)
+
+    return lines;
+}
+
+export const boardMap = string2BoardMap(lvl0);
+
+// export const boardMap = [
+//     [" ", " ", "#", "#", "#", "#", "#",],
+//     ["#", "#", "#", "P", ".", ".", "#",],
+//     ["#", ".", ".", "B", "#", ".", "#", "#"],
+//     ["#", ".", "#", "G", "B", ".", ".", "#"],
+//     ["#", ".", ".", "G", ".", "#", ".", "#"],
+//     ["#", ".", ".", "#", ".", ".", ".", "#"],
+//     [" ", "#", ".", ".", ".", "#", "#", "#"],
+//     [" ", "#", "#", "#", "#", "#",]
+// ]
 
 const NUM_ROWS = boardMap.length;
-const NUM_COLS = boardMap[0].length;
 
 export function buildGameBoard() {
     const pieces = {
@@ -18,17 +37,19 @@ export function buildGameBoard() {
     };
 
     let numberOfGoals = 0;
-    
+
     const game = document.getElementById('game');
     const board = createGameElement('div', 'board', game);
 
     for (let i = 0; i < NUM_ROWS; i++) {
         const linha = createGameElement('div', 'row', board);
+        const NUM_COLS = boardMap[i].length;
+
         for (let j = 0; j < NUM_COLS; j++) {
             const celula = createGameElement('div', 'cell', linha);
             const char = boardMap[i][j];
             const position = { x: j, y: i };
-            
+
             if (char === '#') celula.classList.add('wall');
             // if (char === 'B') celula.classList.add('box');
             if (char === 'P') pieces.player = position;
@@ -37,9 +58,11 @@ export function buildGameBoard() {
                 celula.classList.add('goal');
                 numberOfGoals++;
             }
+            if (char === ' ') celula.classList.add('empty');
+            if (char === '_') celula.classList.add('empty');
         }
     }
-    
+
     return { pieces, numberOfGoals };
 }
 

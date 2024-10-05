05ee41e5998d0e75455be3f78328b2e40f04ab80
Renan Maziero <renanmaziero07@gmail.com>
2024-10-05 05:20:21 -0300

    Aumenta ms da mensagem de vitória para 500

--- src/script.js
+++ src/script.js
@@ -69,7 +69,7 @@ function handlePieceMovement(keycode) {
 
             if (caixasCertas === numberOfGoals) {
                 setTimeout(applyVictoryStyles, 200);
-                setTimeout(showVictoryMessage, 400);
+                setTimeout(showVictoryMessage, 500);
             }
             console.log("Box Moves:", boxMoves += 1);
             boxMovesElement.textContent = boxMoves;
